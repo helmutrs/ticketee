31ad3f11e0e9112ed94bab84082ca80e5ffe8eba
Helmut Reiterer <helmutrs@mac.com>
2013-08-05 14:30:01 -0400

    Added feature for signing in via Devise-provide form

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,5 +1,8 @@
 Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
+    unconfirmed = attributes.delete("unconfirmed") == "true" # this will delete the attribute unconfirmed from first
+    # scenario on the signing_in.feature but it's also make a variable unconfirmed true (2 in one action)
     @user = User.create!(attributes)
+    @user.confirm! unless unconfirmed  # this is work for the second scenario check rails 3 in action page 152.
   end
 end
