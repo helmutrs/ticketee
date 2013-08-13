881d0ef0c7e2f7b2379c351a62931a7bbac7b88a
Helmut Reiterer <helmutrs@mac.com>
2013-08-12 22:55:32 -0400

    Added the ability to edit, delete, and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,13 @@
 class Admin::UsersController < Admin::BaseController #inheritance
+  before_filter :find_user,  :only => [:show, :edit, :destroy, :update]
+  before_filter :admin_status, :only => [:update, :create]
+
   def index
     @users = User.all(:order => "email")
   end
 
   def show
-
+    find_user
   end
 
   def new
@@ -12,17 +15,52 @@ class Admin::UsersController < Admin::BaseController #inheritance
   end
 
   def create
-    params[:user].delete("admin") == "1" ? admin = "1" : admin = "0"
     @user = User.new(params[:user])
 
     if @user.save
       flash[:notice] = "User has been created."
       #@user.update_attribute("admin", admin) if admin
-      @user.update_attribute("admin", admin) if admin == "1"
+      @user.update_attribute("admin", @admin) if @admin == "1"
       redirect_to admin_users_path
     else
       flash[:alert] = "User has not been created."
       render :action => 'new'
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User has been updated."
+      @user.update_attribute("admin", @admin) if @admin == "1"
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been updated."
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:notice] = "User has been deleted."
+    redirect_to admin_users_path
+
+  end
+
+  private
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def admin_status
+    params[:user].delete(:admin) == "1" ? @admin = "1" : @admin = "0"
   end
 end
